1c9be63534e0897a421c50ef1c1f815be0de2b1d
Pavel Petrovich <gaara33377@gmail.com>
2019-03-26 11:26:08 +0300

    vector module

--- js/main.js
+++ js/main.js
@@ -639,55 +639,6 @@ Items.prototype.setSprite = function (load, cordSpriteX, cordsSpriteY,
 }
 
 
-function Vector(x, y) {
-
-    let _that = this;
-
-    /* current coords */
-    this.x = x || 0;
-    this.y = y || 0;
-
-}
-
-Vector.prototype.multiply = function (vector) {
-
-    return new Vector(this.x * vector, this.y * vector);
-}
-
-Vector.prototype.add = function (vector) {
-
-    /* прибавляю найденные координаты к координатам объекта*/
-    return new Vector(this.x + vector.x, this.y + vector.y);
-}
-
-Vector.prototype.divide = function (vector) {
-    return new Vector(this.x / vector, this.y / vector);
-}
-
-Vector.prototype.normalize = function (vector) {
-
-    /*
-     * нормализую вектор.
-     * Преобразование заданного вектора в вектор
-     * в том же направлении, но с единичной длиной.
-     */
-
-    return this.divide(this.length())
-}
-
-Vector.prototype.length = function (vector) {
-
-    /*  длина вектора  */
-    return Math.sqrt(this.dot(this));
-}
-
-Vector.prototype.dot = function (vector) {
-
-    /* Скалярное произведение векторов в 2D пространстве */
-    return this.x * vector.x + this.y * vector.y;
-}
-
-
 function update(time, gamer, load, game, link) {
 
     gamer.gameTime += time; // game time
