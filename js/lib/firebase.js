e76e165180925804a28e7fbc20e8fb41f4cc8675
Pavel Petrovich <gaara33377@gmail.com>
2019-08-06 01:31:14 +0300

    Updates

--- js/lib/firebase.js
+++ js/lib/firebase.js
@@ -1,16 +1,17 @@
-// import * as firebase from 'firebase/app';
-// import firebaseConfig from './lib/firebase';
+import * as firebase from 'firebase/app';
+const firestore = require('firebase/firestore');
 
-
-// const configFirebase = {
-//     apiKey: "AIzaSyDbccidJ1gkbHlOarCaPQxLZB663DuEkoM",
-//       authDomain: "game-e444e.firebaseapp.com",
-//       databaseURL: "https://game-e444e.firebaseio.com",
-//       projectId: "game-e444e",
-//       storageBucket: "game-e444e.appspot.com",
-//       messagingSenderId: "1087306837571"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
-// export {db};
+const config = (()=>{
+    const configFirebase = {
+    apiKey: "AIzaSyDbccidJ1gkbHlOarCaPQxLZB663DuEkoM",
+      authDomain: "game-e444e.firebaseapp.com",
+      databaseURL: "https://game-e444e.firebaseio.com",
+      projectId: "game-e444e",
+      storageBucket: "game-e444e.appspot.com",
+      messagingSenderId: "1087306837571"
+};
+ return configFirebase;
+})();
+firebase.initializeApp(config());
+const db = firebase.firestore();
+export {db};
